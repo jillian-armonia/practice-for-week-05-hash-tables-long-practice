d1f1abbdf5ad4ad7a9fb16082a916cda8190fdad
Jillian Armonia <jillian.marice@gmail.com>
2024-08-27 21:07:20 +0900

    Finished practice problems

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,25 +1,70 @@
 function anagrams(str1, str2) {
-  // Your code here
+  const stringSet1 = new Set(str1);
+
+  if (str1.length !== str2.length) return false;
+
+
+  for (let i = 0; i < str2.length; i++){
+    if (!stringSet1.has(str2[i])) return false;
+  }
+
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  const set1 = new Set(arr1);
+  let filtered = arr2.filter(n => set1.has(n));
+
+  return filtered;
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  let set = new Set();
+
+  for (let i = 0; i < arr.length; i++){
+    if (set.has(arr[i])){
+      return arr[i];
+    }
+
+    set.add(arr[i]);
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+  let set = new Set(nums);
+  let setKeys = set.keys();
+  let constant = setKeys.next().value;
+
+  while(constant !== undefined){
+    if (target - constant !== constant && set.has(target - constant)) return true;
+
+    constant = setKeys.next().value;
+  }
+
+  return false;
 }
 
-
 function wordPattern(pattern, strings) {
-  // Your code here
+  let stringSet = new Set(strings).keys();
+  let string = stringSet.next().value;
+
+  let stringObject = new Object();
+  let codeLetter = 65;
+
+  while(string !== undefined){
+    stringObject[String.fromCharCode(codeLetter)] = string;
+    string = stringSet.next().value;
+    codeLetter++;
+  }
+
+  for (let i = 0; i < pattern.length; i++){
+    if (stringObject[pattern[i]] !== strings[i]) return false;
+  }
+
+  return true;
 }
 
 
